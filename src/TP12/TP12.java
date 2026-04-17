16806a7014f12a979b22483b4634ca877a22a78e
Konstantinos VANAKAS <2508kovanakas@student.epfc.eu>
2026-04-17 12:49:30 +0200

    end of lesson 12

--- src/TP12/TP12.java
+++ src/TP12/TP12.java
@@ -1,5 +1,7 @@
 package TP12;
 
+import eu.epfc.prm.Array;
+
 import java.util.Scanner;
 
 public class TP12 {
@@ -13,12 +15,24 @@ public class TP12 {
 
 
     public static void main(String[] args) {
-        Date d = new Date();
-        Personne p = new Personne();
-//        saisie(d);
-//        afficheD(d);
-        saisie(p);
-        afficheP(p);
+        Array<Personne> table = new Array<Personne>();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Combien de personnes voules vous enregister? : ");
+        int n = s.nextInt();
+        while(n>0){
+            Date d = new Date();
+            Personne p = new Personne();
+            saisie(p);
+            table.add(p);
+            n--;
+        }
+
+        for (int i = 0; i < table.size(); i++) {
+            afficheP(table.get(i));
+        }
+        System.out.println("_____________________________________________________________________________");
+        getyounger(table);
+
     }
     public static void saisie (Date d){
         Scanner s = new Scanner(System.in);
@@ -29,12 +43,9 @@ public class TP12 {
         System.out.println(m3);
         d.annee = s.nextInt();
     }
-    public static void afficheD(Date d){
-        System.out.println(d.jour+"/"+d.mois+"/"+d.annee);
-    }
-    public static void afficheP(Personne p){
-        System.out.print(p.nom+ " "+p.prenom+" "); afficheD(p.ddn);
-    }
+    public static void afficheD(Date d){System.out.print(d.jour+"/"+d.mois+"/"+d.annee);}
+    public static void afficheP(Personne p){System.out.print(p.nom+ " "+p.prenom+" "); afficheD(p.ddn);
+        System.out.println(" ");}
 
     public static void saisie(Personne p){
         Scanner s = new Scanner(System.in);
@@ -44,6 +55,46 @@ public class TP12 {
         p.prenom = s.nextLine();
         System.out.println(m6);
         p.ddn = new Date();
+        System.out.println(" ");
         saisie(p.ddn);
     }
+    public static int compare(Date d1,Date d2){
+        int date1,date2;
+        int j1 = d1.jour;
+        int m1= d1.mois*100;
+        int a1 = d1.annee*10000;
+        date1= a1+m1+j1;
+        int j2 = d2.jour;
+        int m2= d2.mois*100;
+        int a2 = d2.annee*10000;
+        date2= a2+m2+j2;
+        return date1-date2;
+    }
+
+    public static int compareAge(Personne p1, Personne p2){
+        return compare(p1.ddn,p2.ddn);
+    }
+
+    public static int compareNomPrenom(Personne p1, Personne p2){
+        String per1 = p1.nom +" "+p1.prenom;
+        String per2 = p2.nom +" "+p2.prenom;
+        return per1.compareTo(per2);
+   }
+
+   public static void getyounger(Array<Personne> table) {
+       Personne res = null;
+       if (!table.isEmpty()) {
+           res = table.get(0);
+           int n = table.size()-1;
+           while (n >= 1) {
+               if (compareAge(res, table.get(n)) < 0)
+                   res = table.get(n);
+               n--;
+           }
+
+       }
+       afficheP(res);
+   }
+
+
 }
